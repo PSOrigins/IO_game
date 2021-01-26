501221df823385704a19ea8cfa7b9431997dfb7c
PSOrigins <serapinaspijus@gmail.com>
2021-01-26 17:49:07 +0200

    2021.01.26
    Istrinti libro, obj, sources,
    Pridetas analog controlleris

--- IO_game/Assets/Scripts/PlayerController.cs
+++ IO_game/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance;
+    public Transform AnalogUI;
+
     private Rigidbody rb;
     private Vector2 ClickPosOrigin;
     private Vector2 ClickPosCur;
@@ -12,6 +15,9 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     private float Speed;
+
+    private const float minClamp = 0f;
+    private const float maxClamp = 50f;
     
     private enum AnalogState
     {
@@ -22,7 +28,13 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        State = AnalogState.Idle;
+        AnalogUI.gameObject.SetActive(false);
         rb = GetComponent<Rigidbody>();
+        if(Instance == null)
+        {
+            Instance = this;
+        }
     }
     void Update()
     {
@@ -30,18 +42,28 @@ public class PlayerController : MonoBehaviour
         {
             State = AnalogState.Usable;
             ClickPosOrigin = Input.mousePosition;
-            rb.MovePosition(rb.position + transform.forward * Speed * Time.deltaTime);
+            //rb.MovePosition(rb.position + transform.forward * Speed * Time.deltaTime);
+            AnalogUI.position = ClickPosOrigin;
+            AnalogUI.gameObject.SetActive(true);
         }
         else if (Input.GetMouseButtonUp(0))
         {
             State = AnalogState.Idle;
+            AnalogUI.gameObject.SetActive(false);
+
         }
+
         switch (State)
         {
             case AnalogState.Usable:
                 ClickPosCur = Input.mousePosition;
                 RotationAngle = Mathf.Atan2(ClickPosCur.x - ClickPosOrigin.x, ClickPosCur.y - ClickPosOrigin.y) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, RotationAngle, 0);
+
+                float distanceBetweenPoints = Mathf.Clamp(Vector2.Distance(ClickPosCur, ClickPosOrigin), minClamp, maxClamp);
+                AnalogUI.GetChild(0).position = ClickPosOrigin + 
+                    new Vector2(ClickPosCur.x - ClickPosOrigin.x, ClickPosCur.y - ClickPosOrigin.y).normalized * distanceBetweenPoints;
+                rb.MovePosition(rb.position + transform.forward * Speed * Time.deltaTime);
                 break;
 
             case AnalogState.Idle:
