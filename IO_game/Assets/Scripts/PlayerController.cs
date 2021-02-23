ce52b535659cad7bd0e218ed2659970689b7397e
PSOrigins <serapinaspijus@gmail.com>
2021-02-23 17:59:56 +0200

    2021.02.02/2021.02.09/2021.02.23
    Pilna visko...

--- IO_game/Assets/Scripts/PlayerController.cs
+++ IO_game/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
+    public static int minionCount { get; set; }
     public Transform AnalogUI;
+    public TextMeshProUGUI ScoreText;
 
     private Rigidbody rb;
     private Vector2 ClickPosOrigin;
     private Vector2 ClickPosCur;
     private float RotationAngle;
     private AnalogState State;
+    private Animator _playerAnim;
 
     [SerializeField]
     private float Speed;
+    
 
     private const float minClamp = 0f;
     private const float maxClamp = 50f;
@@ -28,13 +33,14 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        ScoreText.text = "0";
         State = AnalogState.Idle;
         AnalogUI.gameObject.SetActive(false);
         rb = GetComponent<Rigidbody>();
         if(Instance == null)
-        {
             Instance = this;
-        }
+        _playerAnim = GetComponentInChildren<Animator>();
+        
     }
     void Update()
     {
@@ -42,7 +48,6 @@ public class PlayerController : MonoBehaviour
         {
             State = AnalogState.Usable;
             ClickPosOrigin = Input.mousePosition;
-            //rb.MovePosition(rb.position + transform.forward * Speed * Time.deltaTime);
             AnalogUI.position = ClickPosOrigin;
             AnalogUI.gameObject.SetActive(true);
         }
@@ -53,21 +58,30 @@ public class PlayerController : MonoBehaviour
 
         }
 
+        
+    }
+    void FixedUpdate()
+    {
         switch (State)
         {
             case AnalogState.Usable:
+                _playerAnim.SetBool("IsRunning", true);
                 ClickPosCur = Input.mousePosition;
                 RotationAngle = Mathf.Atan2(ClickPosCur.x - ClickPosOrigin.x, ClickPosCur.y - ClickPosOrigin.y) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, RotationAngle, 0);
-
                 float distanceBetweenPoints = Mathf.Clamp(Vector2.Distance(ClickPosCur, ClickPosOrigin), minClamp, maxClamp);
-                AnalogUI.GetChild(0).position = ClickPosOrigin + 
+                AnalogUI.GetChild(0).position = ClickPosOrigin +
                     new Vector2(ClickPosCur.x - ClickPosOrigin.x, ClickPosCur.y - ClickPosOrigin.y).normalized * distanceBetweenPoints;
                 rb.MovePosition(rb.position + transform.forward * Speed * Time.deltaTime);
                 break;
 
             case AnalogState.Idle:
+                _playerAnim.SetBool("IsRunning", false);
                 break;
         }
+    }
+
+    public void AddPerson(){
+        ScoreText.text = minionCount.ToString();
     }
 }
